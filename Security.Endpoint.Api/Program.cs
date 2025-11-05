b2cda41a64d0ad4ea870c30ceac3c66c89f3fc53
mghiasvand <mghiasvand@mafpars.com>
2025-11-05 07:54:17 +0330

    bugfix in docker-compose build and run

--- Security.Endpoint.Api/Program.cs
+++ Security.Endpoint.Api/Program.cs
@@ -4,7 +4,7 @@ using Security.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApiService(builder.Configuration,builder.Host);
+builder.Services.AddApiService(builder.Configuration, builder.Host);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddInfrastructuresServices(builder.Configuration);
 builder.RegisterApp();
